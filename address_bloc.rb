1c9e7e64c0761854cf4d99de53b8fa66c9f5c6b6
devonbahary <devonbahary@gmail.com>
2017-06-29 14:53:18 +0000

    5 Address Bloc: Menu | Completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,15 @@
+# address_bloc.rb
+
+require_relative 'controllers/menu_controller'
+
+
+# create a controller instance
+menu = MenuController.new
+
+# clear the command line
+system "clear"
+
 puts "Welcome to AddressBloc!"
+
+# display the menu
+menu.main_menu
---
+++ controllers/menu_controller.rb
@@ -0,0 +1,111 @@
+# menu_controller.rb
+
+require_relative '../models/address_book'
+
+class MenuController
+  attr_reader :address_book
+  
+  def initialize
+    @address_book = AddressBook.new
+  end
+  
+  # Main Menu 
+  def main_menu
+    puts "Main Menu - #{address_book.entries.count} entries"
+    puts "1 - View all entries"
+    puts "2 - Create an entry"
+    puts "3 - Search for an entry"
+    puts "4 - Import entries from a CSV"
+    puts "5 - Exit"
+    print "Enter your selection: "
+    
+    selection = gets.to_i
+    
+    # selection processing and loop
+    case selection
+      when 1 # View all entries
+        system "clear"
+        view_all_entries
+        main_menu
+      when 2 # Create an entry
+        system "clear"
+        create_entry
+        main_menu
+      when 3 # Search for an entry
+        system "clear"
+        search_entries
+        main_menu
+      when 4 # Import entries from a CSV
+        system "clear"
+        read_csv
+        main_menu
+      when 5 # Exit
+        puts "Good-bye!"
+        exit(0) # '(0)' signals the program is exiting w/o error
+      else 
+        system "clear"
+        puts "Sorry, that is not a valid input"
+        main_menu
+    end
+  end
+  
+  # view entries one-at-a-time w/'entry_submenu' processing
+  def view_all_entries
+    address_book.entries.each do |entry|
+      system "clear"
+      puts entry.to_s
+      entry_submenu(entry)
+    end
+    
+    system "clear"
+    puts "End of entries"
+  end
+  
+  # take in user input and 'add_entry' to 'address_book'
+  def create_entry
+    system "clear"
+    puts "New AddressBloc Entry"
+    print "Name: "
+    name = gets.chomp
+    print "Phone number: "
+    phone_number = gets.chomp
+    print "Email: "
+    email = gets.chomp
+    
+    address_book.add_entry(name, phone_number, email)
+    
+    system "clear"
+    puts "New entry created"
+  end
+  
+  def search_entries
+  end
+  
+  def read_csv
+  end
+  
+  # 'view_all_entries' single entry processing
+  def entry_submenu(entry)
+    # submenu options
+    puts "n - next entry"
+    puts "d - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+    
+    selection = gets.chomp
+    
+    case selection
+      when 'n'
+      when 'd'
+      when 'e'
+      when 'm'
+        system "clear"
+        main_menu
+      else
+        system "clear"
+        puts "#{selection} is not a valid input"
+        entry_submenu(entry)
+    end
+  end
+  
+end
